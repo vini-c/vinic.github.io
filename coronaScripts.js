75fa98ed7994c857cf27039d340d61befa6f850f
Vinicius <viniciuspeixotorocha@gmail.com>
2020-04-15 13:48:03 -0300

    WidthChecker on Load
    Ajustes de onLoad e função para checar responsividade

--- coronaScripts.js
+++ coronaScripts.js
@@ -1,26 +1,59 @@
-var homeVNCoronaPage = document.getElementById("coronaHome");
+var homeVNCoronaPage = document.getElementById("Informacoes-importantes");
+var coronaMediaScreen = window.matchMedia("(max-width: 800px)")
+var hash =  window.location.hash;
+
+console.log(hash);
+function coronaWidthCheck(pagehash,originContent,clonedContent,realContent,tabNumber){
+  if (coronaMediaScreen.matches) {
+    changeCoronaPage(pagehash,originContent,clonedContent);
+  } else {
+    openCoronaTabsonLoad(realContent,tabNumber);
+  }
+}
 
 window.onload = function(){
- var hash =  window.location.hash;
- if ( hash == "#coronaAtendimentoPage") {
-   changeCoronaPage('coronaAtendimentoPage','coronaAtendimentoOrigin','coronaClonedAtendimentoContent');
- }
- else if ( hash == "#coronaBrasileirosPage") {
-   changeCoronaPage('coronaBrasileirosPage','coronaBrasileirosOrigin','coronaClonedBrasileirosContent');
- }
- else if ( hash == "#coronaRulesPage") {
-   changeCoronaPage('coronaRulesPage','coronaRegrasOrigin','coronaClonedRegrasContent');
-   coronaTopRules();
- }
- else if ( hash == "#coronaReembolsoPage") {
-   changeCoronaPage('coronaReembolsoPage','coronaReembolsoOrigin','coronaClonedReembolsoContent');
- }
- else {
-   window.location.hash = "#coronaHome"
- }
+  var hash =  window.location.hash;
+  if ( hash == "#Atendimento")
+    {
+    coronaWidthCheck('Atendimento','coronaAtendimentoOrigin','coronaClonedAtendimentoContent','coronaAtendimentoContent','Tabs1');
+    }
+   else if
+   ( hash == "#Brasileiros-no-exterior")
+     {
+       coronaWidthCheck('Brasileiros-no-exterior','coronaBrasileirosOrigin','coronaClonedBrasileirosContent','coronaBrasileirosContent','Tabs2');
+     }
+   else if
+   ( hash == "#Regras-de-cias")
+     {
+       coronaWidthCheck('Regras-de-cias','coronaRegrasOrigin','coronaClonedRegrasContent','coronaRegrasContent','Tabs3');
+     }
+   else if
+   ( hash == "#Reembolso")
+     {
+       coronaWidthCheck('Reembolso','coronaReembolsoOrigin','coronaClonedReembolsoContent','coronaReembolsoContent','Tabs4');
+     }
+   else {
+     window.location.hash = "#Informacoes-importantes"
+   }
 };
 
-function openCoronaTabs(evtCorona, coronaContentType) {
+function openCoronaTabsonLoad(coronaContentType, tabNumber) {
+  var i;
+  var x = document.getElementsByClassName("coronaTabsContents");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  coronaTabsLinks = document.getElementsByClassName("coronaTabsCTA");
+  for (i = 0; i < x.length; i++) {
+    coronaTabsLinks[i].className = coronaTabsLinks[i].className.replace(" coronaActiveTab", "");
+  }
+  document.getElementById(coronaContentType).style.display = "block";
+  document.getElementById(tabNumber).classList.add("coronaActiveTab")
+}
+
+
+
+function openCoronaTabs(evtCorona, coronaContentType, coronaPageName) {
   var i;
   var x = document.getElementsByClassName("coronaTabsContents");
   for (i = 0; i < x.length; i++) {
@@ -32,7 +65,9 @@ function openCoronaTabs(evtCorona, coronaContentType) {
   }
   document.getElementById(coronaContentType).style.display = "block";
   evtCorona.currentTarget.className += " coronaActiveTab";
+  window.location.hash = coronaPageName;
 }
+
 function changeCoronaPage(coronaPageName, coronaContentOrigin, coronaClonedContent) {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
@@ -42,7 +77,16 @@ function changeCoronaPage(coronaPageName, coronaContentOrigin, coronaClonedConte
   var coronaClone = coronaDaddyClone.cloneNode(true);
   document.getElementById(coronaClonedContent).appendChild(coronaClone);
   window.location.hash = coronaPageName;
-  console.log(coronaPageName);
+}
+function changeCoronaPageInitial(coronaPageName, coronaContentOrigin, coronaClonedContent) {
+  document.getElementById(coronaClonedContent).innerHTML = "";
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  homeVNCoronaPage.style.display = "none";
+  document.getElementById(coronaPageName).style.display = "block";
+  var coronaDaddyClone = document.getElementById(coronaContentOrigin);
+  var coronaClone = coronaDaddyClone.cloneNode(true);
+  document.getElementById(coronaClonedContent).appendChild(coronaClone);
 }
 function coronaBackHome(coronaPageName, coronaClonedContent) {
   homeVNCoronaPage.style.display = "block";
@@ -50,11 +94,12 @@ function coronaBackHome(coronaPageName, coronaClonedContent) {
   document.getElementById(coronaClonedContent).innerHTML = "";
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-  window.location.hash = "#coronaHome";
+  window.location.hash = "#Informacoes-importantes";
 }
 function coronaModalVN(coronaPageModal) {
   var coronaModal = document.getElementById(coronaPageModal);
   document.getElementById(coronaPageModal).style.display = "block";
+  window.location.hash = coronaPageModal;
   document.onkeydown = function(evt) {
     evt = evt || window.event;
     var isEscape = false;
@@ -68,9 +113,10 @@ function coronaModalVN(coronaPageModal) {
     }
   };
 }
-function coronaCloseModal(coronaPageModal) {
+function coronaCloseModal(coronaPageModal, coronaPageName) {
   var coronaModal = document.getElementById(coronaPageModal);
   document.getElementById(coronaPageModal).style.display = "none";
+  window.location.hash = coronaPageName;
 }
 function coronaTopRules(){
 coronaScrolLCTA = document.getElementById("coronaScrolltoTop");
@@ -115,8 +161,12 @@ function topCoronaFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-window.onhashchange = function() {
-  if (window.location.hash == "#coronaHome") {
-      location.reload()
-  }
-}
+
+// window.onhashchange = function() {
+//   if (window.location.hash == "#Prioridade-2")
+//   {
+//       changeCoronaPageInitial('Regras-de-cias','coronaRegrasOrigin','coronaClonedRegrasContent');
+//       coronaTopRules();
+//       coronaModalVN('Prioridade-2')
+//   }
+// }
